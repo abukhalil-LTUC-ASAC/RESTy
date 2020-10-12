d28efb942011447415e60aa33873a008d57ee0ab
AbuKhalil95 <AbuKhalil95@users.noreply.github.com>
2020-10-12 13:17:02 +0000

    deploy: 1114078b878879f0a7996665c21801412b60ca71

---
+++ precache-manifest.fe5b6b4e66bc467997716e7ac12a25a1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7a219c9de3c252a8e6ca3b8b41c6ecc",
+    "url": "/repo-name/index.html"
+  },
+  {
+    "revision": "0ece2078985f682d8321",
+    "url": "/repo-name/static/css/main.06d518e0.chunk.css"
+  },
+  {
+    "revision": "2c983a220da37da7f158",
+    "url": "/repo-name/static/js/2.3cb76820.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/repo-name/static/js/2.3cb76820.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0ece2078985f682d8321",
+    "url": "/repo-name/static/js/main.2aeb52a2.chunk.js"
+  },
+  {
+    "revision": "3d6ead55a0c2ba577b76",
+    "url": "/repo-name/static/js/runtime-main.b94f04ef.js"
+  },
+  {
+    "revision": "7cba38e7a6f267124506621f9884d5bd",
+    "url": "/repo-name/static/media/SansitaSwashed-Regular.7cba38e7.ttf"
+  }
+]);
