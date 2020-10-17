741d9c51ca58c6a99ac9ce80600f4489f7f7f015
AbuKhalil95 <AbuKhalil95@users.noreply.github.com>
2020-10-17 00:19:04 +0000

    deploy: 4585ee0901475252f26188f36eb7f5512d8aca2e

---
+++ precache-manifest.6a9028c670fba2154bee678513f80075.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c70108b08732120becd29cb8794d8221",
+    "url": "/.%20/index.html"
+  },
+  {
+    "revision": "b57287cf60192fb9df29",
+    "url": "/.%20/static/css/main.274370ba.chunk.css"
+  },
+  {
+    "revision": "87c5e2672f1e324c919f",
+    "url": "/.%20/static/js/2.e7331a40.chunk.js"
+  },
+  {
+    "revision": "13b685b3db9ae92c2d07c213e5e3506f",
+    "url": "/.%20/static/js/2.e7331a40.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b57287cf60192fb9df29",
+    "url": "/.%20/static/js/main.88f1b23a.chunk.js"
+  },
+  {
+    "revision": "e2ecf3d4b51abe81bcba",
+    "url": "/.%20/static/js/runtime-main.324dcdfd.js"
+  },
+  {
+    "revision": "7cba38e7a6f267124506621f9884d5bd",
+    "url": "/.%20/static/media/SansitaSwashed-Regular.7cba38e7.ttf"
+  },
+  {
+    "revision": "5cdf188d4f6cda244c60c7ae665bef07",
+    "url": "/.%20/static/media/loading.5cdf188d.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.04f99194f55189e8f59dac1cd0535213.js"
+  "/.%20/precache-manifest.6a9028c670fba2154bee678513f80075.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/.%20/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
